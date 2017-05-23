5b3d08fbde581f48cceabb0fb46c489540eeb92d
Guilherme Barbosa <nguilhermebarbosa@gmail.com>
2017-05-23 16:11:24 -0300

    Adicionadas as páginas da Laura

---
+++ pages/Report.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class pages_Report : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAprovar1_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar2_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar3_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar4_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar5_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar6_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar7_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar8_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar9_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar10_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar11_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar12_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar13_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar14_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAprovar15_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReprovar16_Click(object sender, EventArgs e)
+    {
+
+    }
+}
