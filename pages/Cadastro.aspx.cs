0819dfabaf37986d460eaaa7d815d04c83fd3301
Guilherme Barbosa <nguilhermebarbosa@gmail.com>
2017-06-20 16:03:13 -0300

    tudo atualizado

---
+++ pages/Cadastro.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class pages_Cadastro : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void rbTipoConta_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (Free.Visible != true)
+        {
+            Free.Visible = true;
+            Premium.Visible = false;
+        }
+        else
+        {
+            Free.Visible = false;
+            Premium.Visible = true;
+        }
+        
+
+    }
+}
