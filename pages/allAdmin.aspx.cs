a46cb744f85e4a8907b39b7771687ae481baab26
Laura <Laura@LF-PC>
2017-06-30 17:01:24 -0300

    aaa

--- pages/allAdmin.aspx.cs
+++ pages/allAdmin.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class pages_allAdmin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
+
+    
 }
