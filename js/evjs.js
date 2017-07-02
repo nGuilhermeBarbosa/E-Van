9aa0c428ae5a487fbf44eb810202bb842cd0a285
Guilherme Barbosa <nguilhermebarbosa@gmail.com>
2017-07-02 17:13:51 -0300

    Assessibilidade para daltônicos (quando logado fica em perfil)

---
+++ js/evjs.js
@@ -0,0 +1,24 @@
+﻿function mudaTamanhoFonte() {
+    document.getElementById("teste").style.fontSize = "40px";
+}
+
+function mudaBackground() {
+    document.getElementById("bgChange").style.backgroundColor = "#f9b946";
+    document.getElementsById("undo").style.visibility = true;
+}
+
+function undoBg() {
+    document.getElementById("bgChange").style.backgroundColor = "#009ffb";
+}
+
+function mudaItalico() {
+    document.getElementById("teste").style.fontStyle = "italic";
+}
+
+function escondeDiv() {
+    document.getElementById("teste").style.display = "none";
+}
+
+function reapareceDiv() {
+    document.getElementById("teste").style.display = "block";
+}
