3d926cac2bcd27538c2658d5bfb814520b0d2ec9
Laura <Laura@LF-PC>
2017-07-02 18:00:11 -0300

    add serviço mot

--- pages/CadastroServiçoMotorista.aspx.cs
+++ pages/CadastroServiçoMotorista.aspx.cs
@@ -11,4 +11,30 @@ public partial class pages_CadastroServiço : System.Web.UI.Page
     {
 
     }
+
+    protected void btnCadastrar_Click(object sender, EventArgs e)
+    {
+        ser_servicos serv = new ser_servicos();
+        serv.Ser_datacadastro = txtData.Text;
+        serv.Ser_origem = txtOrigem.Text;
+        serv.Ser_destino = txtDestino.Text;
+        serv.Ser_descricao = txtDescricao.Text;
+
+
+
+        switch (ser_servicosDB.Insert(serv))
+        {
+            case 0:
+                lblMsg.Text = "Cadastrado com sucesso";
+                break;
+            case -2:
+                lblMsg.Text = "ERRO";
+                break;
+        }
+
+        txtOrigem.Text = "";
+        txtDestino.Text = "";
+        txtData.Text = "";
+        txtDescricao.Text = "";
+    }
 }
