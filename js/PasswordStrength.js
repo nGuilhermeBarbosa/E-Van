bc7373325a135cae5c672865ce390f8243b9b233
Guilherme Barbosa <nguilhermebarbosa@gmail.com>
2017-11-07 20:29:23 -0200

    Requer mínimo 6 caracteres na senha para se cadastrar

--- js/PasswordStrength.js
+++ js/PasswordStrength.js
@@ -4,6 +4,8 @@
     //TextBox left blank.
     if (password.length == 0) {
         password_strength.innerHTML = "";
+        $("#btnCadastrar").prop('disabled', true);
+        $("#feedback").show();
         return;
     }
 
@@ -31,7 +33,35 @@
     //Display status.
     var color = "";
     var strength = "";
-    switch (passed) {
+    if (password.length < 6) {
+        strength = "A sua senha requer no mínimo 6 caracteres";
+        color = "red";
+        $("#btnCadastrar").prop('disabled', true);
+        $("#feedback").show();
+    } else {
+        if (passed == 1) {
+            strength = "A sua senha está fraca, recomendamos melhora-la";
+            color = "red";
+            $("#btnCadastrar").prop('disabled', true);
+            $("#feedback").show();
+        } else if (passed == 2) {
+            strength = "Senha Boa";
+            color = "darkorange";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        } else if (passed == 4) {
+            strength = "Senha Forte";
+            color = "green";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        } else if (passed == 5) {
+            strength = "Senha Muito Forte";
+            color = "darkgreen";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        }
+    }
+    /*switch (passed) {
         case 0:
         case 1:
             strength = "A sua senha está fraca, recomendamos melhora-la";
@@ -58,7 +88,7 @@
             $("#btnCadastrar").prop('disabled', false);
             $("#feedback").hide();
             break;
-    }
+    }*/
     password_strength.innerHTML = strength;
     password_strength.style.color = color;
     password_strength.style.fontWeight = "bold";
@@ -97,7 +127,36 @@ function CheckPasswordStrength2(password) {
     //Display status.
     var color = "";
     var strength = "";
-    switch (passed) {
+    if (password.length < 6){
+        strength = "A sua senha requer no mínimo 6 caracteres";
+        color = "red";
+        $("#btnCadastrar").prop('disabled', true);
+        $("#feedback").show();
+    } else {
+        if (passed == 1) {
+            strength = "A sua senha está fraca, recomendamos melhora-la";
+            color = "red";
+            $("#btnCadastrar").prop('disabled', true);
+            $("#feedback").show();
+        } else if (passed == 2) {
+            strength = "Senha Boa";
+            color = "darkorange";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        } else if (passed == 4) {
+            strength = "Senha Forte";
+            color = "green";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        } else if (passed == 5) {
+            strength = "Senha Muito Forte";
+            color = "darkgreen";
+            $("#btnCadastrar").prop('disabled', false);
+            $("#feedback").hide();
+        }
+    }
+    
+    /*switch (passed) {
         case 0:
         case 1:
             strength = "A sua senha está fraca, recomendamos melhora-la";
@@ -124,7 +183,7 @@ function CheckPasswordStrength2(password) {
             $("#btnCadastrar").prop('disabled', false);
             $("#feedback").hide();
             break;
-    }
+    }*/
     password_strength.innerHTML = strength;
     password_strength.style.color = color;
     password_strength.style.fontWeight = "bold";
