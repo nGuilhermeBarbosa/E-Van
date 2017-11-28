4bf1ea3b13e2a37578618108a48fa435912543c4
Laura <Laura@LF-PC>
2017-11-28 11:25:22 -0200

    vizualizar todos veiculos

--- pages/TodosVeiculos.aspx.cs
+++ pages/TodosVeiculos.aspx.cs
@@ -17,19 +17,20 @@ public partial class pages_TodosVeiculos : System.Web.UI.Page
             //hdf.Value = usu.id.ToString();
             CadastroCompleto();
         }
+        VeiculoLoad();
     }
-    DataSet ds = mot_motoristaDB.SelectAll();
+    DataSet ds = tra_transporteDB.SelectAll();
 
     public void VeiculoLoad()
     {
         foreach (DataRow dados in ds.Tables[0].Rows)
         {
             Literal.Text += "<div class='serviceBox shadow'> "
-                + "<div class='title'>" + dados["pes_nome"] + "</div>"
+                + "<div class='title'>" + dados["tra_modelo"] + "</div>"
                 + "<div class='padding'>"
-                + "<b>Sexo</b>: " + dados["pes_sexo"] + "<br />"
-                + "<b>Cidade</b>:" + dados["pes_cidade"] + "<br />"
-                + "<b>Contato</b>:" + dados["usu_email"] + "<br />"
+                + "<b>Lugares Disponiveis: </b> " + dados["tra_lugares"] + "<br />"
+               // + "<b>Cidade</b>:" + dados["pes_cidade"] + "<br />"
+               // + "<b>Contato</b>:" + dados["usu_email"] + "<br />"
                 + "</div></div>";
         }
     }
